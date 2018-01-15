a30c8d2cb2cbec93c92865001c21d959b758cfae
lsubrama <lakshmi_subramanian@premierinc.com>
2018-01-15 11:15:28 +0800

    Initial commit

---
+++ src/main/scala/com/hsbc/peak/events/streaming/StreamingConfig.scala
@@ -0,0 +1,33 @@
+package com.hsbc.peak.events.streaming
+
+/**
+  * Created by lsubrama on 1/4/18.
+  */
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.Duration
+
+/**
+  * StreamingConfig with parameters for Kinesis.
+  */
+case class StreamingConfig(
+
+                            streamName:         String,
+                            region:             String,
+                            checkpointInterval: Duration,
+                            initialPosition:    InitialPositionInStream,
+                            storageLevel:       StorageLevel,
+                            appName:            String,
+                            master:             String,
+                            batchInterval:      Duration,
+                            awsProfile:         String,
+                            hdfsOutputLoc:      String   ,
+                            s3OutputLoc:        String,
+                            checkpointDirectory: String
+
+                          ) {
+  /**
+    * The Kinesis endpoint from the region.
+    */
+  val endpointUrl = s"https://kinesis.${region}.amazonaws.com"
+}
